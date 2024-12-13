edf710c36873b97922beba49839a7363780ed144
El Ghali <ben-ghali@hotmail.com>
2024-12-13 01:03:44 +0100

    feat: change data fetching from recursive to allow async chore: refactor the code for clarity

--- backshot.py
+++ backshot.py
@@ -1,58 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
+}
 
-def get_text(keyword, chapter, number):
-    """
-    Get the text of a specified chapter of a novel given its keyword.
-    """
-
-    number = int(number)
-    chapter = int(chapter)
-
-    url = f"https://novelfull.com/search?keyword={keyword}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
-    }
-
+def get_html(url):
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    page = chapter // 50 + 1
-    state = False
-    for page_num in range(page-1, page + 2):
-        url = f"https://novelfull.com{soup.select('div.list-truyen a')[0]['href']}?page={page_num}"
-
-        response = requests.get(url, headers=headers)
-        soup1 = BeautifulSoup(response.text, 'html.parser')
+    return BeautifulSoup(response.text, 'html.parser')
 
 
-        for title, link in zip(
-            [item.text for item in soup1.select('#list-chapter .row li a')],
-            [item['href'] for item in soup1.select('#list-chapter li a')]
-        ):
-            if str(chapter) in title.split('-')[0]:
-                url = f"https://novelfull.com{link}"
-                state = True
-                break
-        if state:
-            break
-
-    texts = []
-    for _ in range(number):
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        text = '\n'.join(
+def get_page_content(url):
+    soup = get_html(url)
+    return '\n'.join(
             [
                 item.text
                 for item in soup.select('p')
                 if item.text.strip() and 'translator' not in item.text.lower() and 'copyright' not in item.text.lower()
             ]
         )
-        url = f"https://novelfull.com{soup.select('a.btn.btn-success')[1]['href']}"
-        texts.append(text)
 
-    text = '\n'.join(texts)
+
+def get_urls(number, chapter, keyword):
+    number = int(number)
+    chapter = int(chapter)
+
+    url = f"https://novelfull.com/search?keyword={keyword}"
+
+    soup = get_html(url)
+
+    novel_title = soup.select_one('.truyen-title a').text
+    page = chapter // 50 + 1
+
+
+    state = False
+    for page_num in range(page-1, page + 2):
+        url_part1 = f'https://novelfull.com/infinite-mana-in-the-apocalypse.html?page='
+        url = f"{url_part1}{page_num}"
+        soup1 = get_html(url)
+        titles = [item.text.split('-')[0] for item in soup1.select('#list-chapter .row li a')]
+        links = [item['href'] for item in soup1.select('.list-chapter li a')]
+        # print(titles)
+        for index, (title, link) in enumerate(zip(titles, links)):
+            if str(chapter) in title:
+                last_page = (index + number)//50 + page_num
+                last_index = (index + number)%50 +(last_page - page_num)*50
+                for page in range(page_num+1, last_page + 1):
+                    url = f"{url_part1}{page}"
+                    soup1 = get_html(url)
+                    links += [item['href'] for item in soup1.select('.list-chapter li a')]
+                
+                links = [f"https://novelfull.com{link}" for link in links[index:last_index]]
+                state = True
+                novel_image = "https://novelfull.com"+soup1.select_one('.book img')['src']
+                break
+        if state:
+            break
+    
+    return links, novel_title, novel_image
+
+
+def preprocess(text):
     text = text.replace('.a.p.e', 'ape').replace('l.a.p', 'lap').replace('+', ' plus')
     text = text.replace('.a.r.e', 'are').replace('.o.a.n', 'oan').replace('.a.t.u.r.e', 'ature')
     text = text.replace('.r.e.a.s.t', 'reast')
@@ -64,6 +72,28 @@ def get_text(keyword, chapter, number):
     text = text.replace('.u.m', 'um').replace('.u.s.t', 'ust').replace('.e.x', 'ex')
     text = text.replace('.d.u.l.t', 'dult')
     text = text.replace('.i.c.k', 'ick').replace('.r.o.s.t.i.t.u.t.e', 'rostitute')
-    text = text.replace('+', 'plus')
+    return text
 
-    return {'url': url, 'text': text}
+
+def get_text(keyword, chapter, number):
+    
+    urls, novel_title, novel_image = get_urls(number, chapter, keyword)
+
+    texts = []
+    for url in urls:
+        texts.append(get_page_content(url))
+
+    text = '\n'.join(texts)
+    text = preprocess(text)
+
+    text_array = [par.strip() for par in text.split("\n")]
+    text_formatted = "".join([f'<p id="par{i}">{par}</p>' for i,par in enumerate(text_array)])
+
+    return {
+        'text': text,
+        "title":novel_title,
+        "image":novel_image,
+        "formatted":text_formatted,
+        "array":text_array
+        }
+
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
-@app.route("/kherya/", methods=["POST"])
+@app.route("/api/", methods=["POST"])
 def get_user():
     print(request.get_json())
     keyword = request.get_json()["keyword"]
     chapter = request.get_json()["chapter"]
     number = request.get_json()["number"]
     user_data = get_text(keyword, chapter, number)
+    print(user_data["image"])
     return jsonify(user_data), 200
 
 if __name__ == "__main__":
